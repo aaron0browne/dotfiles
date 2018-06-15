475f549abf108b51e2009f7b91b250588a6f3c95
Browne <brownea@8c8590d2e032.chop.edu>
2018-06-14 21:14:27 -0400

    Add more fonts and remove unneeded stuff
    Signed-off-by: Browne <brownea@8c8590d2e032.chop.edu>

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,8 +1,2 @@
-[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
-if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
-    export GPG_AGENT_INFO
-    export SSH_AUTH_SOCK
-    export SSH_AGENT_PID
-else
-    eval "$( gpg-agent --daemon --write-env-file ~/.gpg-agent-info )"
-fi
+gpg-connect-agent updatestartuptty /bye
+export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
